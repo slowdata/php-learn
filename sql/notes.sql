438f18180f1f4159503697f4e5c9daa4d437ddc0
Ricardo Dias <ricardo.j.dias@pgr.pt>
2023-03-01 17:50:51 +0000

    sql code to create some dammy data

---
+++ sql/notes.sql
@@ -0,0 +1,8 @@
+INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES
+(6, 'Ideas for next vacation', 3);
+INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES
+(7, 'Next art project research', 4);
+INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES
+(8, 'Work reminders...', 3);
+INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES
+(9, 'Design techniques blog post', 4);
---
+++ sql/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO `users` (`id`, `name`, `email`) VALUES
+(3, 'John', 'john@example.com');
+INSERT INTO `users` (`id`, `name`, `email`) VALUES
+(4, 'Kate', 'kate@example.com');
