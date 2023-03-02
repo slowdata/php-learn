6adfe411970efc5a145fdebe525616c470983b1c
Ricardo Dias <ricardo.j.dias@pgr.pt>
2023-03-02 10:28:51 +0000

    Added script to create tables

--- sql/notes.sql
+++ sql/notes.sql
@@ -1,3 +1,14 @@
+CREATE TABLE `notes` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `body` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_id` bigint unsigned NOT NULL,
+  UNIQUE KEY `id` (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+
 INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES
 (6, 'Ideas for next vacation', 3);
 INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES
--- sql/users.sql
+++ sql/users.sql
@@ -1,3 +1,14 @@
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `users_email_idx` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+
 INSERT INTO `users` (`id`, `name`, `email`) VALUES
 (3, 'John', 'john@example.com');
 INSERT INTO `users` (`id`, `name`, `email`) VALUES
